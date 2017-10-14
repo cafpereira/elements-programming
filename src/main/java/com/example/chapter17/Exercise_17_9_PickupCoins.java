57d3178e27e2b0b629bb839ae7b8370f1c1a25b6
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2017-10-14 09:17:42 -0400

    question 17.5 search 2D matrix (fixed v2)

---
+++ src/main/java/com/example/chapter17/Exercise_17_9_PickupCoins.java
@@ -0,0 +1,13 @@
+package com.example.chapter17;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
+
+class Exercise_17_9_PickupCoins {
+  // TODO
+}
