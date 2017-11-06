e003c4342e867a7dd45ccc381618181d1634efb9
Carlos Pereira <carlos_pereira@ultimatesoftware.com>
2017-11-06 03:36:04 -0500

    question 17.9 pickup coins game

--- src/main/java/com/example/chapter17/Exercise_17_5_Search2DArray_v2.java
+++ src/main/java/com/example/chapter17/Exercise_17_5_Search2DArray_v2.java
@@ -1,12 +1,6 @@
 package com.example.chapter17;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 class StringInMatrixV2 {
 
--- src/main/java/com/example/chapter17/Exercise_17_9_PickupCoins.java
+++ src/main/java/com/example/chapter17/Exercise_17_9_PickupCoins.java
@@ -3,5 +3,38 @@ package com.example.chapter17;
 import java.util.*;
 
 class Exercise_17_9_PickupCoins {
-  // TODO
+
+  public static int maxCoinsRec(int[] coins) {
+    return maxCoinsRec(coins, 0, coins.length - 1, true);
+  }
+
+  /**
+   *
+   if start > end:
+     return 0
+
+   int a = coin[start] + min( max_coin( coin, start+2,end ), max_coin( coin, start+1,end-1 ) )
+   int b = coin[end] + min( max_coin( coin, start+1,end-1 ), max_coin( coin, start,end-2 ) )
+
+   return max(a,b)
+   */
+  private static int maxCoinsRec(int[] coins, int start, int end, boolean player) {
+    if (end - start == 1) {
+      return player ? Math.max(coins[start], coins[end]) : Math.min(coins[start], coins[end]);
+    }
+    int left = maxCoinsRec(coins, start + 1, end, !player);
+    int right = maxCoinsRec(coins, start, end - 1, !player);
+
+    if (player) {
+      int scoreLeft = left + coins[start];
+      int scoreRight = right + coins[end];
+      return Math.max(scoreLeft, scoreRight);
+    } else {
+      return Math.min(left, right);
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println("maxCoinsRec(coins) = " + maxCoinsRec(new int[]{5, 25, 10, 1}));
+  }
 }
